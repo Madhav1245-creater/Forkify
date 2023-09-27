0f408d2cf20ef89129f751765a7af9ffb48db303
Madhav1245-creater <72183611+Madhav1245-creater@users.noreply.github.com>
2023-09-27 15:04:02 +0530

    Update controller.js

--- src/js/controller.js
+++ src/js/controller.js
@@ -7,8 +7,8 @@ import paginationView from './views/paginationView.js';
 import bookmarksView from './views/bookmarksView.js';
 import addRecipeView from './views/addRecipeView.js';
 
-import '/core-js/stable';
-import '/regenerator-runtime/runtime';
+import '/core-js/stable.js';
+import '/regenerator-runtime/runtime.js';
 
 if (module.hot) {
   module.hot.accept;
